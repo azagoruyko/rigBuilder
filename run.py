2083a67ba4cc84db96c2c15929e5481b69c3d72e
azagoruyko <azagoruyko@gmail.com>
2026-04-17 18:26:12 +0300

    fix: capture std out/err on app show only

--- run.py
+++ run.py
@@ -6,9 +6,6 @@ import sys
 directory = os.path.dirname(__file__)
 sys.path.append(os.path.dirname(directory))
 
-from rigBuilder.logger import setupStreamRedirection
-setupStreamRedirection()
-
 from rigBuilder.qt import QApplication, QColor, QPalette, QSharedMemory, QMessageBox
 
 def updatePalette(app: QApplication):
@@ -19,20 +16,23 @@ def updatePalette(app: QApplication):
     app.setPalette(palette)
 
 if __name__ == "__main__":
-    app = QApplication([])    
-
+    app = QApplication([])
+    
     # Prevent multiple instances
     sharedMemory = QSharedMemory("RigBuilder_Unique_Lock")
     if not sharedMemory.create(1):
         QMessageBox.warning(None, "RigBuilder", "RigBuilder is already running.\n\nOnly one instance is allowed at a time.")
         sys.exit(0)
 
+    from rigBuilder.logger import setupStreamRedirection
     from rigBuilder.ui import mainWindow
     from rigBuilder.ui.utils import applyStylesheet
 
     applyStylesheet(app)
     updatePalette(app)
 
-    mainWindow.show()   
+    mainWindow.show()
+
+    setupStreamRedirection()
 
     app.exec()
