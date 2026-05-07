7aa6f173490eea82b67093477e002d773bbf351e
azagoruyko <azagoruyko@gmail.com>
2026-05-07 09:45:32 +0300

    refactor: fixing the warning title

--- run.py
+++ run.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     # Prevent multiple instances
     sharedMemory = QSharedMemory("RigBuilder_Unique_Lock")
     if not sharedMemory.create(1):
-        QMessageBox.warning(None, "RigBuilder", "RigBuilder is already running.\n\nOnly one instance is allowed at a time.")
+        QMessageBox.warning(None, "Rig Builder", "RigBuilder is already running.\n\nOnly one instance is allowed at a time.")
         sys.exit(0)
 
     from rigBuilder.logger import setupStreamRedirection, setupExcepthook
